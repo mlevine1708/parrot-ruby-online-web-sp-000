b6b254139eef6dda636d9d89e11fdd2e30bbdccc
Micah Levine <micah.levine@outlook.com>
2019-08-05 19:59:07 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -2,7 +2,7 @@ require_relative './spec_helper'
 require_relative '../parrot.rb' # Code your solution in this file
 
 def parrot(string)
-  string puts "Squawk!"
+  parrot puts "Squawk!"
   "Squawk!"
 end
 
